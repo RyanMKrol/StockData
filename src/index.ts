ed6d372dabba42634b6a20e08a1229d5fb0b3c2f
Ryan Krol <ryankr@hotmail.co.uk>
2020-03-10 23:39:10 +0000

    Update scheduler again to run experiment

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ mailClient.setTo('ryankrol.m@gmail.com')
 
 mailClient.sendMail('Mail client setup!', '')
 
-schedule.scheduleJob('0 33 23 * * *', async () => {
+schedule.scheduleJob('0 42 23 * * *', async () => {
   try {
     await mailClient.sendMail('Beginning stock data update!', '')
 
