8e5cdd5fbfadc704bc67aa9ec493e3f1fec7b41a
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-13 16:50:05 +0100

    Add schedule to fetch-recent script

--- src/scripts/fetchRecent.js
+++ src/scripts/fetchRecent.js
@@ -1,10 +1,13 @@
+import MailSender from 'noodle-email';
 import {
   pipeline, pipelineClosureMethod, DynamoPersistanceQueue, sleep,
 } from 'noodle-utils';
+import schedule from 'node-schedule';
 
 import { fetchIndexTickers, fetchTickerPriceData } from '../fetch';
 import { processAlphavantageApiResponse, limitAlphaVantageApiResponse } from '../process';
 
+import gmailCredentials from '../../credentials/gmail.json';
 import dynamoCredentials from '../../credentials/dynamo.json';
 
 // storing the most recent 30 items
@@ -12,6 +15,10 @@ const NUM_PRICE_ITEMS_TO_STORE = 30;
 
 const MINUTES_BETWEEN_QUEUE_PUSHES = 1;
 const MS_IN_M = 60000;
+
+const mailClient = new MailSender(gmailCredentials);
+mailClient.setFrom('"StockDataUpdater" <ryankrol.m@gmail.com>');
+mailClient.setTo('ryankrol.m@gmail.com');
 
 /**
  * The method to run our program
@@ -41,4 +48,12 @@ async function main() {
   }
 }
 
-main();
+schedule.scheduleJob('0 0 12 * * *', async () => {
+  try {
+    await mailClient.sendMail('StockDataUpdater Started!', '');
+    await main();
+    await mailClient.sendMail('StockDataUpdater Finished!', '');
+  } catch (error) {
+    await mailClient.sendMail('StockDataUpdater Failed!', error.toString());
+  }
+});
